3132d8ccc9a38e7edb118853902be8f7f5b11e3c
Blue-code-boost <blue.code.boost@gmail.com>
2025-07-30 23:53:54 -0500

    primer test

--- boost-website/js/main.js
+++ boost-website/js/main.js
@@ -86,3 +86,17 @@ function closeBoostPlanModal() {
   modal.classList.remove('show');
   modal.style.display = 'none';
 }
+
+// Scroll suave para los links del navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
